3d56c47ea17dcba9b49f6b4cbb3dc91ecbfb0679
Yang Yang <jokerkeny@gmail.com>
2018-12-06 12:13:25 -0500

    multi_predict

--- predict.py
+++ predict.py
@@ -1,5 +1,5 @@
-from os import listdir
-from os.path import isfile, join
+from os import listdir, makedirs
+from os.path import isfile, join, splitext, exists
 import argparse
 import h5py
 from keras.models import load_model
@@ -7,9 +7,12 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 from scipy import misc
+
 import network
+from preprocess import remove_if_exist
 
 edge = (33 - 21) // 2
+
 
 def ycbcr2rgb(im):
     xform = np.array([[1, 0, 1.402], [1, -0.34414, -.71414], [1, 1.772, 0]])
@@ -17,6 +20,40 @@ def ycbcr2rgb(im):
     rgb[:,:,[1,2]] -= 128
     return rgb.dot(xform.T)
 
+def predict_mul():
+    model = network.srcnn((None, None, 1))
+    model.load_weights(option.model)
+
+    
+    LR_dir=option.LR_dir
+    SR_dir=option.SR_dir+"_nopad"
+    if not exists (SR_dir):
+        makedirs(SR_dir)
+    for fi in listdir(LR_dir):
+        f = join(LR_dir, fi)
+        if not isfile(f):
+            continue
+        X = misc.imread(f, mode='YCbCr')
+
+        srext='.png'
+        sr_file = splitext(fi)[0]+srext
+        sr_file = join(SR_dir, sr_file)
+        remove_if_exist(sr_file)
+
+        # w, h, c = X.shape
+
+        scaled = misc.imresize(X, option.scale/1.0, 'bicubic')
+        newshape=list(scaled.shape)
+        newshape[0]-=2*edge
+        newshape[1]-=2*edge
+        newimg = np.zeros(newshape)
+
+        newimg[:, :, 0, None] = model.predict(scaled[None, :, :, 0, None] / 255)
+        newimg[:, :, 1, None] = model.predict(scaled[None, :, :, 1, None] / 255)
+        newimg[:, :, 2, None] = model.predict(scaled[None, :, :, 2, None] / 255)
+        newimg=ycbcr2rgb(newimg*255)
+        misc.imsave(sr_file, newimg)
+        # misc.imsave(fi,ycbcr2rgb(scaled))
 
 def predict():
     model = network.srcnn((None, None, 1))
@@ -80,7 +117,19 @@ if __name__ == '__main__':
                         dest='pad',
                         type=bool,
                         help="does the model padding 0")
+    parser.add_argument('-LR', '--LR_dir',
+                        default='./data/test_set/LR',
+                        dest='LR_dir',
+                        type=str,
+                        help="LR_dir")
+    parser.add_argument('-SR', '--SR_dir',
+                        default='./data/test_set/SR',
+                        dest='SR_dir',
+                        type=str,
+                        help="SR_dir")
+
     option = parser.parse_args()
     if(option.pad):
         edge=0
-    predict()
+    # predict()
+    predict_mul()
